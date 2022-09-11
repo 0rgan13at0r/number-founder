ff9a878c41f412a753bfea6e0fc21acb894be9c5
Organ13at0r <Organ13at0r_official@proton.me>
2022-09-11 18:42:42 +0300

    Some refactoring code

--- src/core/search.py
+++ src/core/search.py
@@ -7,30 +7,24 @@ from time import sleep
 
 
 def look_for_in_clipboard(pattern: str, output_file: str):
-    numbers_count = 0
-
-    for number in re.findall(pattern, pyperclip.paste()):
-        sleep(0.03)
-
-        print(f"Found: +{''.join(number)}")
-        numbers_count += 1
-
-        if output_file:
-            _output(output_file, f"+{''.join(number)}")
-
-    print(f"Done\nNumbers count: {numbers_count}")
+    _find_number(pattern, pyperclip.paste(), output_file)
 
 
 def look_for_in_text_file(pattern: str, file_to: str, output_file: str):
-    numbers_count = 0
 
     if not Path(file_to).exists():
         print("File wasn't founded!!")
         sys.exit(1)
 
     with open(file_to, "r") as file:
-        for number in re.findall(pattern, file.read()):
-            sleep(0.03)
+        _find_number(pattern, file.read(), output_file)
+
+
+def _find_number(pattern: str, func, output_file: str):
+    numbers_count = 0
+
+    for number in re.findall(pattern, func):
+            sleep(0.05)
 
             print(f"Found: +{''.join(number)}")
             numbers_count += 1
@@ -38,15 +32,9 @@ def look_for_in_text_file(pattern: str, file_to: str, output_file: str):
             if output_file:
                 _output(output_file, f"+{''.join(number)}")
 
-        print(f"Done\nNumbers count: {numbers_count}")
+    print(f"Done\nNumbers count: {numbers_count}")
 
 
 def _output(file_to: str, data: str):
     with open(file_to, "w") as file:
         file.write(data) 
-
-
-# if __name__ == "__main__":
-#     reg = re.compile(r"\+(\d{3})(\s|\S)?(\d{2})(\s|\S)?(\d{3})(\s|\S)?(\d{2})(\s|\S)?(\d{2})")
-#     look_for_in_clipboard(reg)
-#     look_for_in_text_file(reg, "src/core/temp.txt")
